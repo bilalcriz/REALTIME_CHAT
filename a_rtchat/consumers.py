a50d79cc79f64bb576bf16373fda0abee3ab5d79
Bilal <a.bilal292001@gmail.com>
2025-11-01 19:28:23 +0530

    ws with group broadcasting with mutiple user

--- a_rtchat/consumers.py
+++ a_rtchat/consumers.py
@@ -10,7 +10,21 @@ class ChatroomConsumer(WebsocketConsumer):
     self.user = self.scope['user']
     self.chatroom_name = self.scope['url_route']['kwargs']['chatroom_name'] 
     self.chatroom = get_object_or_404(ChatGroup, group_name=self.chatroom_name)
+    
+    # Join the chatroom group
+    async_to_sync(self.channel_layer.group_add)(
+        self.chatroom_name,
+        self.channel_name
+    )
+    
     self.accept()   
+  
+  def disconnect(self, close_code):
+    # Leave the chatroom group
+    async_to_sync(self.channel_layer.group_discard)(
+        self.chatroom_name,
+        self.channel_name
+    )
 
         
   def receive(self, text_data):
@@ -22,47 +36,24 @@ class ChatroomConsumer(WebsocketConsumer):
         author = self.user, 
         group = self.chatroom 
     )
+    
+    # Broadcast message to all users in the group
+    event = {
+        'type': 'message_handler',
+        'message_id': message.id,
+    }
+    async_to_sync(self.channel_layer.group_send)(
+        self.chatroom_name, event
+    )
+  
+  def message_handler(self, event):
+    """Handler for broadcasting messages to all users in the group"""
+    message_id = event['message_id']
+    message = ChatMessage.objects.get(id=message_id)
     context = {
         'message': message,
         'user': self.user,
-      #  'chat_group': self.chatroom
     }
-    html = render_to_string("a_rtchat/partials/chat_message_p.html", context = context)
-    self.send(text_data=html) 
-    #     event = {
-    #         'type': 'message_handler',
-    #         'message_id': message.id,
-    #     }
-    #     async_to_sync(self.channel_layer.group_send)(
-    #         self.chatroom_name, event
-    #     )
-        
-    # def message_handler(self, event):
-    #     message_id = event['message_id']
-    #     message = GroupMessage.objects.get(id=message_id)
-    #     context = {
-    #         'message': message,
-    #         'user': self.user,
-    #         'chat_group': self.chatroom
-    #     }
-    #     html = render_to_string("a_rtchat/partials/chat_message_p.html", context=context)
-    #     self.send(text_data=html)
-        
-        
-    # def update_online_count(self):
-    #     online_count = self.chatroom.users_online.count() -1
-        
-    #     event = {
-    #         'type': 'online_count_handler',
-    #         'online_count': online_count
-    #     }
-    #     async_to_sync(self.channel_layer.group_send)(self.chatroom_name, event)
-        
-    # def online_count_handler(self, event):
-    #     online_count = event['online_count']
-        
-    #     chat_messages = ChatGroup.objects.get(group_name=self.chatroom_name).chat_messages.all()[:30]
-    #     author_ids = set([message.author.id for message in chat_messages])
-    #     users = User.objects.filter(id__in=author_ids)
-        
+    html = render_to_string("a_rtchat/partials/chat_message_p.html", context=context)
+    self.send(text_data=html)
 
